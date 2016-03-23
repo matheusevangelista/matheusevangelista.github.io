9b5098bae1a9b2db1cf463d839b521d93c3d86f5
Matheus Evangelista <matheus.evangelista@jchebly.com.br>
2016-03-23 16:13:57 -0300

    first commit

---
+++ js/default.js
@@ -0,0 +1,21 @@
+$(document).ready(function($) {
+
+        setTimeout(function(){
+
+            $('#txtPortais').arctext({radius : 170, dir : -1, animation  : {
+                            speed   : 600,
+                            easing  : 'ease-out'
+                        }});
+
+            $('#txtAplicativos').arctext({radius : 170, dir : -1, animation  : {
+                            speed   : 600,
+                            easing  : 'ease-out'
+                        }});
+
+
+
+
+        }, 100);
+
+
+});
